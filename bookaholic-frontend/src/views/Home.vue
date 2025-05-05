d703504ca5ea67b4d8e5492f07da663a2e82ed9b
wscneto <contato.walter@proton.me>
2025-05-04 21:38:22 -0300

    updates Home page

--- bookaholic-frontend/src/views/Home.vue
+++ bookaholic-frontend/src/views/Home.vue
@@ -5,25 +5,5 @@
         <h1 class="text-4xl font-bold text-primary mb-8">
             Welcome to Bookaholic
         </h1>
-
-        <div class="flex gap-6">
-            <router-link
-                :to="{ name: 'auth', query: { login: 'true' } }"
-                class="bg-white border-2 border-primary text-primary px-6 py-3 rounded-xl text-lg font-semibold hover:opacity-90 hover:bg-primary hover:text-white transition"
-            >
-                Login
-            </router-link>
-
-            <router-link
-                :to="{ name: 'auth', query: { login: 'false' } }"
-                class="bg-white border-2 border-primary text-primary px-6 py-3 rounded-xl text-lg font-semibold hover:opacity-90 hover:bg-primary hover:text-white transition"
-            >
-                Register
-            </router-link>
-        </div>
     </div>
 </template>
-
-<script lang="ts" setup></script>
-
-<style scoped></style>
