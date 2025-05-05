334421eda271dd7019671b1eab792f9263a6d667
wscneto <contato.walter@proton.me>
2025-05-05 17:43:17 -0300

    modified BookCard component and BooksCarousel to redirect user to book page

--- bookaholic-frontend/src/components/BookCard.vue
+++ bookaholic-frontend/src/components/BookCard.vue
@@ -1,23 +1,30 @@
 <template>
-    <div class="p-4 bg-white rounded-lg shadow-md w-64">
-        <img
-            :src="book.cover_url"
-            alt="Book Cover"
-            class="w-full h-64 object-cover rounded-md mb-3"
-        />
-        <h3 class="text-lg font-bold mb-1">{{ book.title }}</h3>
-        <p class="text-sm text-gray-700">
-            {{ truncatedDescription }}
-        </p>
-    </div>
+    <router-link :to="`/books/${book.id}`">
+        <div class="p-4 bg-white rounded-lg shadow-md w-64">
+            <img
+                :src="book.cover_url"
+                alt="Book Cover"
+                class="w-full h-64 object-cover rounded-md mb-3"
+            />
+            <h3 class="text-lg font-bold mb-1">{{ book.title }}</h3>
+            <p class="text-sm text-gray-700">
+                {{ truncatedDescription }}
+            </p>
+            <p class="text-sm font-bold text-gray-700 mt-3">
+                ${{ book.price }}
+            </p>
+        </div>
+    </router-link>
 </template>
 
 <script lang="ts" setup>
     import { computed } from "vue";
     const props = defineProps<{
         book: {
+            id: number;
             title: string;
             description: string;
+            price: number;
             cover_url: string;
         };
     }>();
--- bookaholic-frontend/src/components/BooksCarousel.vue
+++ bookaholic-frontend/src/components/BooksCarousel.vue
@@ -8,23 +8,29 @@
         :autoplayInterval="3000"
     >
         <template #item="slotProps">
-            <div class="p-4 bg-white rounded-lg shadow-md w-64">
-                <img
-                    :src="slotProps.data.cover_url"
-                    alt="Book Cover"
-                    class="w-full h-64 object-cover rounded-md mb-3"
-                />
-                <h3 class="text-lg font-bold mb-1">
-                    {{ slotProps.data.title }}
-                </h3>
-                <p class="text-sm text-gray-700">
-                    {{
-                        slotProps.data.description.length > 100
-                            ? slotProps.data.description.slice(0, 100) + "..."
-                            : slotProps.data.description
-                    }}
-                </p>
-            </div>
+            <router-link :to="`/books/${slotProps.data.id}`">
+                <div class="p-4 bg-white rounded-lg shadow-md w-64">
+                    <img
+                        :src="slotProps.data.cover_url"
+                        alt="Book Cover"
+                        class="w-full h-64 object-cover rounded-md mb-3"
+                    />
+                    <h3 class="text-lg font-bold mb-1">
+                        {{ slotProps.data.title }}
+                    </h3>
+                    <p class="text-sm text-gray-700">
+                        {{
+                            slotProps.data.description.length > 100
+                                ? slotProps.data.description.slice(0, 100) +
+                                  "..."
+                                : slotProps.data.description
+                        }}
+                    </p>
+                    <p class="text-sm font-bold text-gray-700 mt-3">
+                        ${{ slotProps.data.price }}
+                    </p>
+                </div>
+            </router-link>
         </template>
     </Carousel>
 </template>
