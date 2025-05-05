bb6e1c790a8cbee4545f62030fcc0b7cdc4c09db
wscneto <contato.walter@proton.me>
2025-05-04 21:27:54 -0300

    created Footer component to show informations at the bottom of the screen

---
+++ bookaholic-frontend/src/components/Footer.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="footer">
+        <span
+            >Made by
+            <a
+                href="https://github.com/wscneto"
+                target="_blank"
+                class="footer-link text-primary"
+                >Walter Neto</a
+            ></span
+        >
+    </div>
+</template>
+
+<style scoped>
+    .footer {
+        background-color: #222;
+        color: white;
+        text-align: center;
+        padding: 10px;
+        font-size: 14px;
+        margin-top: 20px;
+    }
+
+    .footer a {
+        text-decoration: none;
+    }
+
+    .footer a:hover {
+        text-decoration: underline;
+    }
+</style>
