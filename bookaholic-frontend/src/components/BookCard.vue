e83a524351e65151c378b630e41768d426cb6832
wscneto <contato.walter@proton.me>
2025-05-04 21:27:16 -0300

    created BookCard component to render books

---
+++ bookaholic-frontend/src/components/BookCard.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="p-4 bg-white rounded-lg shadow-md w-64">
+        <img
+            :src="book.cover_url"
+            alt="Book Cover"
+            class="w-full h-64 object-cover rounded-md mb-3"
+        />
+        <h3 class="text-lg font-bold mb-1">{{ book.title }}</h3>
+        <p class="text-sm text-gray-700">
+            {{ truncatedDescription }}
+        </p>
+    </div>
+</template>
+
+<script lang="ts" setup>
+    import { computed } from "vue";
+    const props = defineProps<{
+        book: {
+            title: string;
+            description: string;
+            cover_url: string;
+        };
+    }>();
+
+    const charLimit = 100;
+
+    const truncatedDescription = computed(() => {
+        return props.book.description.length > charLimit
+            ? props.book.description.slice(0, charLimit) + "..."
+            : props.book.description;
+    });
+</script>
+
+<style scoped>
+    div:hover {
+        transform: translateY(-2px);
+        transition: transform 0.2s ease;
+    }
+</style>
