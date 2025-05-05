713af99732e66ce8a4b2b9cd16ec82a2e61aaa45
wscneto <contato.walter@proton.me>
2025-05-04 21:28:32 -0300

    created NavBar component for better navigation

---
+++ bookaholic-frontend/src/components/NavBar.vue
@@ -0,0 +1,71 @@
+<template>
+    <nav
+        class="sticky top-0 z-50 bg-white border-b border-gray-200 px-4 py-3 flex justify-between items-center shadow-sm"
+    >
+        <router-link
+            to="/"
+            class="text-xl font-bold text-primary"
+            >Bookaholic</router-link
+        >
+
+        <div class="flex items-center gap-4">
+            <form
+                @submit.prevent="onSearch"
+                class="flex items-center gap-3"
+            >
+                <input
+                    v-model="searchTerm"
+                    type="text"
+                    placeholder="Search books"
+                    class="px-5 py-2 rounded border"
+                />
+                <button
+                    type="submit"
+                    class="cursor-pointer bg-primary border-1 border-primary text-white px-5 py-2 rounded hover:opacity-90 font-semibold"
+                >
+                    Search
+                </button>
+            </form>
+            <div
+                v-if="!auth.isLoggedIn"
+                class="flex items-center gap-4"
+            >
+                <router-link
+                    to="/auth?login=true"
+                    class="bg-white border-1 border-primary text-primary px-6 py-3 rounded-xl font-semibold hover:opacity-90 hover:bg-primary hover:text-white transition"
+                    >Login</router-link
+                >
+                <router-link
+                    to="/auth?login=false"
+                    class="bg-white border-1 border-primary text-primary px-6 py-3 rounded-xl font-semibold hover:opacity-90 hover:bg-primary hover:text-white transition"
+                    >Register</router-link
+                >
+            </div>
+            <div v-else>
+                <button
+                    @click="auth.logout()"
+                    class="cursor-pointer bg-white border-1 border-primary text-primary px-6 py-3 rounded-xl font-semibold hover:opacity-90 hover:bg-primary hover:text-white transition"
+                >
+                    Logout
+                </button>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script lang="ts" setup>
+    import { ref } from "vue";
+    import { useRouter } from "vue-router";
+    import { useAuthStore } from "@/stores/auth";
+
+    const auth = useAuthStore();
+    const searchTerm = ref("");
+    const router = useRouter();
+    auth.restore();
+
+    const onSearch = () => {
+        if (searchTerm.value.trim()) {
+            router.push({ name: "search", query: { q: searchTerm.value } });
+        }
+    };
+</script>
